2b8508ad6bd16b27dbfcc1f7c35acf13f415dd6c
edwin webb <edwin.webb@gmail.com>
2018-03-21 10:34:38 -0700

    save progress

--- sparkle.js
+++ sparkle.js
@@ -1,43 +1,34 @@
 class SparklePainter {
 
     constructor() {
-        const pCount = 10;
         this.particles = [];
         this.currentValue = 0;
-        // for(let i = 0; i < pCount; i++) {
-        //     this.particles.push({
-        //         x: 0,
-        //         y: 0,
-        //         a: 1,
-        //         targetX: 0,
-        //         targetY: 0
-        //     })
-        // }
     }
 
-    static get inputProperties() { return ['--sparkle-pos', '--animation-tick']; }
+    static get inputProperties() { return ['--sparkle-pos', '--animation-tick', '--sparkle-color']; }
 
     paint(ctx, geom, properties) {
         const sliderPosition = parseInt(properties.get('--sparkle-pos').toString());
         const tick = parseInt(properties.get('--animation-tick').toString());
+        const sColor = properties.get('--sparkle-color').toString();
         const { width, height } = geom;
         const wPos = width * sliderPosition / 100;
 
         if(this.currentValue !== sliderPosition) {
             for (let index = 0; index < 5; index++) {
                 this.particles.push({
-                    x: wPos,
+                    x: wPos + (Math.random() * 10) - 5,
                     y: height * Math.random() * 0.75
                 });
             }
         }
-        
+
         // draw
         this.particles.map( (p, i) => {
             const alpha = p.y / height;
             p.y -= 0.5;
             ctx.beginPath();
-            ctx.fillStyle = `rgba(255, 0, 0, ${alpha})`;
+            ctx.fillStyle = sColor.replace(')', ','+alpha+')');
             ctx.rect(p.x, p.y, 1, 1);
             ctx.fill();
             return p;
